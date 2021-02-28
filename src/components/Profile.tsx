d3f46843191a7143215683e4082a3c27fa24c773
Flávio M Barbosa <flavio.barbosa@dataprev.gov.br>
2021-02-28 18:04:18 -0300

    aula 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
-    return (
-        <div className={styles.profileContainer}>
-            <img src="https://github.com/flaviobarbosa.png" alt="Flavio Barbosa" />
-            <div>
-                <strong>Flavio Barbosa</strong>
-                <p>
-                    <img src="icons/level.svg" alt="level"/>
-                    Level 1</p>
-            </div>
-        </div>
-    )
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src='https://github.com/flaviobarbosa.png' alt='Flavio Barbosa' />
+      <div>
+        <strong>Flavio Barbosa</strong>
+        <p>
+          <img src='icons/level.svg' alt='level' />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
 }
